a38599abb084d9f35114cd2075bd25083f37a428
mayhmemo <mayhm.social@proton.me>
2026-03-06 17:52:29 -0300

    refactor: adjust viewmodel provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_refatoracao_baguncado/data/repositories/todo_repository_impl.dart';
 import 'package:todo_refatoracao_baguncado/presentation/ui/app_root.dart';
 import 'package:todo_refatoracao_baguncado/presentation/viewmodels/todo_viewmodel.dart';
 
@@ -7,7 +8,7 @@ void main() {
   runApp(
     MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => TodoViewModel()),
+        Provider(create: (_) => TodoViewModel(TodoRepositoryImpl())),
       ],
       child: const AppRoot(),
     ),
