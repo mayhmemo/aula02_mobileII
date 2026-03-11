8243d604ec57c458a4069f91b6be55dcc011d858
mayhmemo <mayhm.social@proton.me>
2026-03-11 10:17:27 -0300

    feat: create new types of AppError

--- lib/core/errors/app_error.dart
+++ lib/core/errors/app_error.dart
@@ -1,7 +1,28 @@
 class AppError implements Exception {
   final String message;
-  const AppError(this.message);
+  final Object? cause;
+
+  const AppError(this.message, [this.cause]);
 
   @override
   String toString() => message;
+}
+
+class NetworkError extends AppError {
+  const NetworkError(super.message, [super.cause]);
+}
+
+class ServerError extends AppError {
+  final int statusCode;
+
+  const ServerError(this.statusCode, {String? message, Object? cause})
+      : super(message ?? 'HTTP $statusCode', cause);
+}
+
+class ParsingError extends AppError {
+  const ParsingError(super.message, [super.cause]);
+}
+
+class StorageError extends AppError {
+  const StorageError(super.message, [super.cause]);
 }
